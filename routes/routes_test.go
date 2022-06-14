6fcdbaec43d4b74e37afcc222a7b37d8fe690a17
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-14 14:30:24 -0300

    fix tests

--- routes/routes_test.go
+++ routes/routes_test.go
@@ -3,8 +3,10 @@ package routes
 import (
 	"crud_tasks/handlers"
 	"crud_tasks/models"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
@@ -34,14 +36,41 @@ func TestGetUserById(t *testing.T) {
 	r := setupTestingRoutes()
 
 	t.Run("test get user by id", func(t *testing.T) {
-		r.GET("/users/id", handlers.GetUserById)
 
 		id, _ := models.InsertUser("test get by id", "getbyid@mail.com", "pass", "a number")
-		url := "/users" + strconv.Itoa(id)
-		req, _ := http.NewRequest("GET", url, nil)
+		userExpect := models.User{
+			ID:       id,
+			Name:     "test get by id",
+			Email:    "getbyid@mail.com",
+			Password: "pass",
+			Phone:    "a number",
+		}
+		defer models.DeleteUserById(id)
+
+		r.GET("/users/:id", handlers.GetUserById)
+
+		req, _ := http.NewRequest("GET", "/users/"+strconv.Itoa(id), nil)
 		res := httptest.NewRecorder()
 		r.ServeHTTP(res, req)
+
+		var user models.User
+		_ = json.Unmarshal(res.Body.Bytes(), &user)
 		assert.Equal(t, http.StatusOK, res.Code)
+		assert.Equal(t, userExpect.Email, user.Email)
 	})
-	//t.Run("test get user by id where id not on db")
+
+	t.Run("test get user by id where id not on db", func(t *testing.T) {
+		r.GET("/users/:id", handlers.GetUserById)
+
+		req, _ := http.NewRequest("GET", "/users/0", nil)
+		res := httptest.NewRecorder()
+		r.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusNotFound, res.Code)
+
+		expect := `{"status":"not found"}`
+		resBody, _ := ioutil.ReadAll(res.Body)
+		got := string(resBody)
+		assert.Equal(t, expect, got, "erro response expected")
+	})
 }
