b73967c965ae683ad567dfa7ad948cbe3b863ad5
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-14 08:48:19 -0300

    create db connection and model

---
+++ database/db.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectWithDB() *sql.DB {
+	dsn := "user=postgres dbname=postgres password=12345678 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return db
+}
---
+++ models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	ID       int
+	Nome     string
+	Email    string
+	Senha    string
+	Telefone string
+}
