150ea43c64aff43dd7b7abab73350d4f0769f355
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-15 13:00:54 -0300

    implement singleton database connection

--- database/db.go
+++ database/db.go
@@ -3,16 +3,29 @@ package database
 import (
 	"database/sql"
 	"log"
+	"sync"
 
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+var once sync.Once
+
 //fazer singleton
-func ConnectWithDB() *sql.DB {
+func createConnection() *sql.DB {
 	dsn := "user=postgres dbname=postgres password=12345678 host=localhost sslmode=disable"
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+	return db
+}
+
+func ConnectWithDB() *sql.DB {
+	if db == nil {
+		once.Do(func() {
+			db = createConnection()
+		})
 	}
 	return db
 }
